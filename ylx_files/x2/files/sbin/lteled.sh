06af86cd5f4ef565397f8f9f3aa9fe21076d225b
zf <zf@iyunlink.com>
2022-03-29 06:03:10 -0400

    lte power up,in /etc/init.d/system

--- ylx_files/x2/files/sbin/lteled.sh
+++ ylx_files/x2/files/sbin/lteled.sh
@@ -13,6 +13,8 @@ sleep 8
 echo "netdev" > /sys/class/leds/lte1led/trigger
 echo "netdev" >  /sys/class/leds/lte2led/trigger
 
+netdevice1=`uci get config4g.4G.netdevice`
+netdevice2=`uci get config4g.4G1.netdevice`
 echo "$netdevice1" > /sys/class/leds/lte1led/device_name
 echo "$netdevice2" > /sys/class/leds/lte2led/device_name
 
